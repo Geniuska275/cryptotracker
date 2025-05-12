025f49e6cc47b475e0f856d0727313eb0df24c38
Geniuska275 <aigbojie2020@gmail.com>
2025-05-12 13:21:16 +0100

    uI UPDATED

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
-module.exports = {
-  distDir: 'build',
-}
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  
+};
+
+export default nextConfig;
